9e3e9f5cc34003b846894d9d0dcb40d9c60e73fb
choijinhap <pppooo95@naver.com>
2022-08-28 20:57:11 +0900

    Components class 수정 - props, mounted추가

--- src/core/Component.js
+++ src/core/Component.js
@@ -1,19 +1,23 @@
 export default class Component {
 	$target;
 	$state;
+	$props;
 
-	constructor($target) {
+	constructor($target, $props) {
 		this.$target = $target;
+		this.$props = $props;
 		this.setup();
 		this.setEvent();
 		this.render();
 	}
 	setup() {}
+	mounted() {}
 	template() {
 		return ``;
 	}
 	render() {
 		this.$target.innerHTML = this.template();
+		this.mounted();
 	}
 	setEvent() {}
 	setState(newState) {
