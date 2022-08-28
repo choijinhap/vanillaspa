0a41df466787653de13cfb237fd722d8eaeb6a8b
choijinhap <pppooo95@naver.com>
2022-08-28 20:59:04 +0900

    Entry point 변경 - App.js -> main.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import Items from './components/Items.js';
-class App {
+export default class App {
 	constructor() {
 		const $app = document.querySelector('#app');
 		new Items($app);
 	}
 }
-new App();
